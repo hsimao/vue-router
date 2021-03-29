2d37d221e7f9be567130a13b967280d93530a640
hsimao <e087754958@gmail.com>
2021-03-29 20:06:02 +0800

    add: EventList 新增 beforeRouteEnter 先取 api 資料完才進入頁面, 並增加進度條效果

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -28,7 +28,7 @@
 <script>
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
-import { watchEffect } from 'vue'
+import NProgress from 'nprogress'
 
 export default {
   name: 'EventList',
@@ -42,19 +42,20 @@ export default {
       totalEvents: 0
     }
   },
-  created() {
-    watchEffect(() => {
-      this.events = null
-      EventService.getEvents(this.prePage, this.page)
-        .then(response => {
-          this.events = response.data
-          this.totalEvents = response.headers['x-total-count']
+  beforeRouteEnter(routeTo, routeFrom, next) {
+    NProgress.start()
+    EventService.getEvents(2, parseInt(routeTo.query.page) || 1)
+      .then(response => {
+        // 透過 next callback 方法可以取得 component this 位置
+        next(comp => {
+          comp.events = response.data
+          comp.totalEvents = response.headers['x-total-count']
         })
-        .catch(error => {
-          console.log(error)
-          this.$router.push({ name: 'NetworkError' })
-        })
-    })
+      })
+      .catch(() => {
+        next({ name: 'NetworkError' })
+      })
+      .finally(NProgress.done())
   },
   computed: {
     hasNextPage() {
